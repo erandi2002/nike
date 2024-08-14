2959a2340c140a77a96c3c5278e33281d6ff90c7
erandi2002 <emunasinghe75@gmail.com>
2024-08-15 02:19:42 +0530

    Create Nav component and implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from "./sections";
+import Nav from "./components/Nav";
+
 const App = () => (
   <main className="relative">
-    Nav
+    < Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      < Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      < PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      < SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      < Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      < SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      < CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      < Subscribe />
     </section>
     <section className="bg-black padding-x padding-t pb-8">
-      Footer
+      < Footer />
     </section>
   </main>
 );
