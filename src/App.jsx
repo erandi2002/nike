206bfad118d470a404bc64ee106a24899b4c54e4
erandi2002 <emunasinghe75@gmail.com>
2024-08-16 21:55:32 +0530

    Complete Hero section with image, text, and statistics display.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
     < Nav />
-    <section className="xl:padding-1 wide:padding-r padding-b">
+    <section className="xl:padding-l wide:padding-r padding-b">
       < Hero />
     </section>
     <section className="padding">
